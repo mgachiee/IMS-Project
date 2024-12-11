4e03decc552602f67788f0e3fe50425ca4a50d19
mgachiee <bobadilla.markallen49@gmail.com>
2024-12-12 00:46:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(express.urlencoded({ extended: true }));
 const session = require('express-session');
 const passport = require('passport');
 const MongoStorage = require('connect-mongo');
-const flash = require('connect-flash');
 require('./config/passport');
 
 app.use(session({
@@ -28,7 +27,6 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(flash());
 
 app.use('/', itemRoutes);
 app.use('/auth', authRoutes);
